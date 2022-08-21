4cdd01dec7094905921874638324e29cb854f57f
Jimbonk <jimbonkgit@gmail.com>
2022-08-21 15:49:36 -0400

    Don't send message to self thread

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ use std::io::{BufRead, BufReader, BufWriter, Result, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
+use std::thread::ThreadId;
 
-fn handle_client(stream: TcpStream, publisher: Sender<String>, consumer: Receiver<String>) {
+type Message = String;
+
+fn handle_client(
+    stream: TcpStream,
+    publisher: Sender<(ThreadId, Message)>,
+    consumer: Receiver<String>,
+) {
     let mut reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
     let mut buffer = String::new();
@@ -21,7 +28,7 @@ fn handle_client(stream: TcpStream, publisher: Sender<String>, consumer: Receive
                     break;
                 } else {
                     publisher
-                        .send(buffer.clone())
+                        .send((thread::current().id(), buffer.clone()))
                         .expect("Error publishing message.");
                     buffer.clear();
                 }
@@ -51,7 +58,10 @@ fn handle_client(stream: TcpStream, publisher: Sender<String>, consumer: Receive
     }
 }
 
-fn start_publisher(publish: Receiver<String>, subscribers: Receiver<Sender<String>>) {
+fn start_publisher(
+    publish: Receiver<(ThreadId, Message)>,
+    subscribers: Receiver<(ThreadId, Sender<Message>)>,
+) {
     let mut subs = Vec::new();
 
     loop {
@@ -59,9 +69,11 @@ fn start_publisher(publish: Receiver<String>, subscribers: Receiver<Sender<Strin
             subs.push(new_sub);
         }
 
-        if let Ok(msg) = publish.try_recv() {
-            for sub in &subs {
-                sub.send(msg.clone()).expect("Failed to broadcast message.");
+        if let Ok((thread_id, msg)) = publish.try_recv() {
+            for (sub_id, sub) in &subs {
+                if *sub_id != thread_id {
+                    sub.send(msg.clone()).expect("Failed to broadcast message.");
+                }
             }
         }
     }
@@ -81,12 +93,12 @@ fn start_server(address: String, port: String) -> Result<()> {
                 println!("New connection: {}", stream.peer_addr().unwrap());
                 let publisher = pub_tx.clone();
                 let (consumer_tx, consumer_rx) = channel();
-                let _thread_id =
+                let thread_id =
                     thread::spawn(move || handle_client(stream, publisher, consumer_rx))
                         .thread()
                         .id();
                 subscribe
-                    .send(consumer_tx)
+                    .send((thread_id, consumer_tx))
                     .expect("Error subscribing thread.");
             }
             Err(msg) => panic!("The stream is borked: {}", msg),
